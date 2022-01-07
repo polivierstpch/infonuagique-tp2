b8b6d855ef8894545557ea75107c13e1ef250ee4
Pier-Olivier St-Pierre-Chouinard <pier.olivier.stpch@gmail.com>
2022-01-07 16:03:39 -0500

    API fichier est fonctionnelle.

--- src/AutoRapide.Vehicules.API/Controllers/GestionVehiculeController.cs
+++ src/AutoRapide.Vehicules.API/Controllers/GestionVehiculeController.cs
@@ -17,16 +17,28 @@ public class GestionVehiculeController : ControllerBase
         _logger = logger;
     }
     
+    /// <summary>
+    /// Retourne tous les véhicules dans la base de données.
+    /// </summary>
+    /// <returns>Une réponse HTTP Ok avec une liste de véhicule dans le corps de celle-ci.</returns>
+    /// <response code="200">Une liste de véhicule a été retournée.</response>
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Vehicule>>> Get()
+    public async Task<ActionResult<IEnumerable<Vehicule>>> ObtenirTout()
     {
         var vehicules = await _vehiculeService.ObtenirListe();
         return Ok(vehicules);
     }
-
+    
+    /// <summary>
+    /// Effectue l'obtention d'un véhicule spécifique dans la base de données selon l'identifiant passé dans le corps de la requête.
+    /// </summary>
+    /// <param name="id">L'identifiant du véhicule à obtenir.</param>
+    /// <returns>Une réponse HTTP Ok avec le véhicule demandé dans le corps de celle-ci.</returns>
+    /// <response code="200">Le véhicule a été trouvé et a été envoyé dans la réponse.</response>
+    /// <response code="404">Le véhicule avec l'identifiant n'a pas été trouvé dans la base de données.</response>
     [HttpGet]
     [Route("{id:int}")]
-    public async Task<ActionResult<Vehicule>> Get(int id)
+    public async Task<ActionResult<Vehicule>> ObtenirUn(int id)
     {
         var vehicule = await _vehiculeService.TrouverParIdAsync(id);
 
@@ -35,18 +47,40 @@ public class GestionVehiculeController : ControllerBase
 
         return Ok(vehicule);
     }
-
+    
+    /// <summary>
+    /// Effectue l'ajout d'un véhicule dans la base de données.
+    /// </summary>
+    /// <param name="vehicule">Le véhicule à ajouter dans la base de données.</param>
+    /// <returns>Une réponse HTTP Created, avec le lien de l'API pour y accéder par l'identifiant.</returns>
+    /// <response code="201">Le véhicule a bien été créé dans la base de données.</response>
+    /// <response code="400">Le véhicule fourni n'est pas valide.</response>
     [HttpPost]
     [Route("enregistrer")]
-    public async Task<IActionResult> Post([FromBody] Vehicule vehicule)
+    public async Task<IActionResult> Ajouter([FromBody] Vehicule vehicule)
     {
+        if (!ModelState.IsValid)
+        {
+            var erreurs = ModelState.Values.SelectMany(v => v.Errors);
+            return BadRequest(erreurs);
+        }
+
         await _vehiculeService.EnregistrerAsync(vehicule);
-        return CreatedAtAction(nameof(Get), new {id = vehicule.Id}, vehicule);
+        return CreatedAtAction(nameof(ObtenirUn), new {id = vehicule.Id}, vehicule);
+
     }
 
+    /// <summary>
+    /// Effectue la suppression d'un véhicule dans la base de données selon l'identifiant passé en paramètre.
+    /// </summary>
+    /// <param name="id">Identifiant du véhicule dans la base de données.</param>
+    /// <returns>Une réponse HTTP NoContent, si la suppression a réussi.</returns>
+    /// <response code="204">L'action de supprimer à bien été reçue.</response>
+    /// <response code="404">Le véhicule avec l'identifiant n'a pas été trouvé dans la base de données.</response>
+    /// <response code="500">Une erreur est survenue lors de la suppression du véhicule dans la base de données.</response>
     [HttpDelete]
     [Route("supprimer/{id:int}")]
-    public async Task<IActionResult> Delete(int id)
+    public async Task<IActionResult> Supprimer(int id)
     {
         var vehiculeASupprimer = await _vehiculeService.TrouverParIdAsync(id);
 
@@ -57,18 +91,37 @@ public class GestionVehiculeController : ControllerBase
 
         return NoContent();
     }
-
+    
+    
+    /// <summary>
+    /// Effectue la modification d'un véhicule dans la base de données selon les valeurs passées dans le corps de la requête.
+    /// </summary>
+    /// <remarks>Aucune remarque.</remarks>
+    /// <param name="id">Identifiant du véhicule dans la base de données.</param>
+    /// <param name="vehicule">Véhicule à modifier.</param>
+    /// <returns>Une réponse HTTP NoContent, si la modification a réussi.</returns>
+    /// <response code="204">L'action de modifier à bien été reçue.</response>
+    /// <response code="400">L'identifiant fourni n'est pas valide selon le véhicule passé.</response>
+    /// <response code="400">Le véhicule fourni n'est pas valide.</response>
+    /// <response code="404">Le véhicule avec l'identifiant n'a pas été trouvé dans la base de données.</response>
+    /// <response code="500">Une erreur est survenue lors de la modification du véhicule dans la base de données.</response>
     [HttpPut]
     [Route("modifier/{id:int}")]
-    public async Task<IActionResult> Put(int id, [FromBody] Vehicule vehicule)
+    public async Task<IActionResult> Modifier(int id, [FromBody] Vehicule vehicule)
     {
         if (id != vehicule.Id)
-            return BadRequest();
+            return BadRequest("L'identifant n'est pas le même que celui du véhicule du corps de la requête.");
 
         var vehiculeExistant = await _vehiculeService.TrouverParIdAsync(id);
 
         if (vehiculeExistant is null)
             return NotFound();
+
+        if (!ModelState.IsValid)
+        {
+            var erreurs = ModelState.Values.SelectMany(v => v.Errors);
+            return BadRequest(erreurs);
+        }
 
         await _vehiculeService.ModifierAsync(vehicule);
 
--- src/AutoRapide.Vehicules.API/Entities/Vehicule.cs
+++ src/AutoRapide.Vehicules.API/Entities/Vehicule.cs
@@ -12,28 +12,41 @@ public class Vehicule
 {
     [Key]
     public int Id { get; set; }
-
+    
+    [Required]
     public string Constructeur { get; set; }
-
+    
+    [Required]
     public string Modele { get; set; }
     
+    [Required]
+    [Range(1900, 9999)]
     public int AnneeFabrication { get; set; }
     
+    [Required]
     public TypeVehicule Type { get; set; }
-
+    
+    [Required]
+    [Range(1, int.MaxValue)]
     public int NombreSiege { get; set; }
-
+    
+    [Required]
     public string Couleur { get; set; }
-
+    
+    [Required]
+    [RegularExpression(@"^[A-Z0-9]{17}$")]
     public string NIV { get; set; }
-
+    
+    [Required]
     public string Image1Url { get; set; }
 
     public string Image2Url { get; set; }
-
+    
     public string Description { get; set; }
-
+    
     public bool EstDisponible { get; set; }
-
+    
+    [Required]
+    [Range(1, double.MaxValue)]
     public double Prix { get; set; }
 }
