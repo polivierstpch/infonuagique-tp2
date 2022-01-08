254574dfbd73a02a5ade8f3a473101a7ce401537
Pier-Olivier St-Pierre-Chouinard <pier.olivier.stpch@gmail.com>
2022-01-08 16:01:14 -0500

    Mise à jour des messages de validation des véhicules.

--- src/AutoRapide.Vehicules.API/Entities/Vehicule.cs
+++ src/AutoRapide.Vehicules.API/Entities/Vehicule.cs
@@ -13,31 +13,31 @@ public class Vehicule
     [Key]
     public int Id { get; set; }
     
-    [Required]
+    [Required(ErrorMessage = "Ce champ est requis.")]
     public string Constructeur { get; set; }
     
-    [Required]
+    [Required(ErrorMessage = "Ce champ est requis.")]
     public string Modele { get; set; }
     
-    [Required]
-    [Range(1900, 9999)]
+    [Required(ErrorMessage = "Ce champ est requis.")]
+    [Range(1900, 9999, ErrorMessage = "Veuillez fournir une date de fabrication valide (1900 à 9999).")]
     public int AnneeFabrication { get; set; }
     
-    [Required]
+    [Required(ErrorMessage = "Ce champ est requis.")]
     public TypeVehicule Type { get; set; }
     
-    [Required]
-    [Range(1, int.MaxValue)]
+    [Required(ErrorMessage = "Ce champ est requis.")]
+    [Range(1, 99, ErrorMessage = "Veuillez fournir un nombre de siège(s) valide (1 à 99).")]
     public int NombreSiege { get; set; }
     
-    [Required]
+    [Required(ErrorMessage = "Ce champ est requis.")]
     public string Couleur { get; set; }
     
-    [Required]
+    [Required(ErrorMessage = "Ce champ est requis.")]
     [RegularExpression(@"^[A-Z0-9]{17}$")]
     public string NIV { get; set; }
     
-    [Required]
+    [Required(ErrorMessage = "Ce champ est requis.")]
     public string Image1Url { get; set; }
 
     public string Image2Url { get; set; }
@@ -46,7 +46,7 @@ public class Vehicule
     
     public bool EstDisponible { get; set; }
     
-    [Required]
-    [Range(1, double.MaxValue)]
+    [Required(ErrorMessage = "Ce champ est requis.")]
+    [Range(1, 9_999_999.99, ErrorMessage = "Veuillez fournir un prix valide. (1,00 à 9,999,999.99)")]
     public double Prix { get; set; }
 }
