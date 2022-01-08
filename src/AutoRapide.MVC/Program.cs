fe15f17ddbdb94611499d8349c2811b4a38f5079
Caroline Marissal-Rousseau <1233968@cegeplimoilou.ca>
2022-01-08 09:22:28 -0500

    Esquisse de mise en place de l'architecture MVC.

--- src/AutoRapide.MVC/Program.cs
+++ src/AutoRapide.MVC/Program.cs
@@ -1,7 +1,22 @@
+//using Utilisateur.API.Data;
+//using Fichiers.API.Data;
+//using Vehicule.API.Data;
+//using Commandes.API.Data;
+//using Favoris.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+//builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<RapidAutoContext>(options =>
+  options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+ 
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
