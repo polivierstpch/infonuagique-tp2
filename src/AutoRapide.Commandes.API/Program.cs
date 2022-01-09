ba20ece5159af348c1e615c1c14a4bafe932892b
Pier-Olivier St-Pierre-Chouinard <pier.olivier.stpch@gmail.com>
2022-01-09 14:20:08 -0500

    Ajout de la configuration OpenAPI pour Commandes API

--- src/AutoRapide.Commandes.API/Program.cs
+++ src/AutoRapide.Commandes.API/Program.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "AutoRapide Commandes API",
+        Description = "Une API pour permettre de gérer une base de données de commandes de véhicules..",
+        Contact = new OpenApiContact
+        {
+            Name = "Pier-Olivier St-Pierre-Chouinard",
+            Url = new Uri("https://github.com/polivierstpch"),
+            Email = "pier.olivier.stpch@gmail.com"
+        }
+        
+    });
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    opt.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 var app = builder.Build();
 
