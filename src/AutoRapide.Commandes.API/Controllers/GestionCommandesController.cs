a098b49a876a1d1bcd1791b6ac3b13f9af36e5da
Pier-Olivier St-Pierre-Chouinard <pier.olivier.stpch@gmail.com>
2022-01-09 14:26:59 -0500

    Ajout du controleur barebones pour Commandes API

---
+++ src/AutoRapide.Commandes.API/Controllers/GestionCommandesController.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoRapide.Commandes.API.Controllers;
+
+[Route("api/commandes")]
+[ApiController]
+public class GestionCommandesController : ControllerBase
+{
+    // GET
+    public async Task<IActionResult> Get()
+    {
+        return Ok();
+    }
+}
