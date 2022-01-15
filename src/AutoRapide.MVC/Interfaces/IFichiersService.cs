be4d03cacf2987c629f244ee951db7be04d299ff
Pier-Olivier St-Pierre-Chouinard <pier.olivier.stpch@gmail.com>
2022-01-15 12:35:26 -0500

    Rendu le service proxy de fichier valide avec les bonnes signatures de méthodes.

--- src/AutoRapide.MVC/Interfaces/IFichiersService.cs
+++ src/AutoRapide.MVC/Interfaces/IFichiersService.cs
@@ -3,6 +3,6 @@
     public interface IFichiersService
     {
         public Task<IEnumerable<string>> EnvoyerFichiers(string niv, IFormFileCollection fichiers);
-        public Task SupprimerFichiers(string nomImage1, string nomImage2);
+        public Task SupprimerFichiers(params string[] urlsImage);
     }
 }
