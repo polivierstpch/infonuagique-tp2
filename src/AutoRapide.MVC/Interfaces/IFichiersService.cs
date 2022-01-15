bb237ae79c2f3c193bbaffe9a36b10605db12ed9
Pier-Olivier St-Pierre-Chouinard <pier.olivier.stpch@gmail.com>
2022-01-15 11:37:48 -0500

    Ajout de l'ajout et la suppression de fichier pour le proxy Fichiers.

--- src/AutoRapide.MVC/Interfaces/IFichiersService.cs
+++ src/AutoRapide.MVC/Interfaces/IFichiersService.cs
@@ -1,6 +1,8 @@
-﻿namespace AutoRapide.MVC.Services
+﻿namespace AutoRapide.MVC.Interfaces
 {
-    public class IFichiersService
+    public interface IFichiersService
     {
+        public Task<IEnumerable<string>> EnvoyerFichiers(string niv, IFormFileCollection fichiers);
+        public Task SupprimerFichiers(string nomImage1, string nomImage2);
     }
 }
